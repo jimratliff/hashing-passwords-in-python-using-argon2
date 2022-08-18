501f5cd7b2a3a398510a4e10319fe99fc8160065
Jim Ratliff <jim@jim-ratliff.name>
2022-08-17 17:10:47 -0700

    Refine app.

--- hashing_passwords_in_python_using_argon2.py
+++ hashing_passwords_in_python_using_argon2.py
@@ -3,19 +3,23 @@ The below roughly follows the sample from the project description of the
 argon2-cffi package on PyPI:
 https://pypi.org/project/argon2-cffi/
 
-See also Svetlin Nakov, Practical Cryptography for Developers, (in process),
-in chapter Argon 2, section “Argon2 Calculation in Python — Example”
-https://cryptobook.nakov.com/mac-and-key-derivation/argon2#argon2-calculation-in-python-example
 """
 
+# The below argon2 module is from the package argon2-cffi
+#   pip install argon2-cffi
+# CFFI stands for “C Foreign Function Interface.”
+# argon2-cffi defends on argon2-cffi-bindings that vendors Argon2’s C code by
+# default. 
+# If something goes wrong, update cffi, pip, and setuptools via:
+#   python -m pip install -U cffi pip setuptools
 import argon2
 
-incoming_password_string = input('Define the password you want to test:\n')
+incoming_password_string = input('\nDefine the password you want to test:\n')
 
 password_hasher = argon2.PasswordHasher()
-encoded_hashed_password = password_hasher.hash(incoming_password_string)
+metadata_encoded_hashed_password = password_hasher.hash(incoming_password_string)
 
-print(f"\nHere is the resulting (encoded) hashed password string:\n{encoded_hashed_password}\n")
+print(f"\nHere is the resulting (metadata-encoded) hashed password string:\n{metadata_encoded_hashed_password}\n")
 
 print("Now for the tests… (Type 'stop' to exit.)\n")
 while True:
@@ -27,11 +31,12 @@ while True:
         break
 
     try:
-        comparison_result = password_hasher.verify(encoded_hashed_password, string_to_compare)
+        # password_hasher.verify(metadata_encoded_hashed_password, string_to_compare)
+        password_hasher.verify("Yippy", string_to_compare)
     except argon2.exceptions.VerifyMismatchError:
         print("❌ NOPE! The string you typed did NOT match the stored password.")
     else:
         print("✅ CORRECT! The string you typed matched the stored password.")
     
-    print("Let’s keep going. (Enter 'stop' to stop.)\n")
+    print("\nLet’s keep going. (Enter 'stop' to stop.)\n")
 
